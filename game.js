ba653f342f23a1198f79c6a85dc7c48bcf50aaae
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-09-26 23:32:42 +0400

    Учет размера клеток

--- game.js
+++ game.js
@@ -5,7 +5,8 @@
       frameX: 23,
       frameY: 26,
       width: 859,
-      height: 861
+      height: 861,
+      cellsize: 44
     },
     "goban13" : {
       size: 13,
@@ -13,7 +14,8 @@
       frameX: 23,
       frameY: 26,
       width: 520,
-      height: 517
+      height: 517,
+      cellsize: 44
     },
     "goban9" : {
       size: 9,
@@ -21,7 +23,8 @@
       frameX: 23,
       frameY: 26,
       width: 353,
-      height: 353
+      height: 353,
+      cellsize: 44
     },
     "goban7" : {
       size: 7,
@@ -29,32 +32,41 @@
       frameX: 23,
       frameY: 29,
       width: 278,
-      height: 273
+      height: 273,
+      cellsize: 44
     }
   };
 
   function Controller(goban) {
+    $(goban).find('#size-switcher').change(this.gobanSizeChange);
+    //console.log($(goban).find('#size-switcher'));
     this.goban = goban;
     this.flash = function(message) {
       $('#flash').text(message);
+    }
+    this.gobanSizeChange = function(e) {
+      console.log(e);
+      game = new Game($('#canvas'), $('#goban'), size);
     }
   }
 
   function ViewGraphics(canvas, size, game) {
     this.goban = gobans["goban" + size];
-    this.context = canvas[0].getContext('2d');
     this.game = game;
     canvas.css('background', 'url(images/goban' + size + '.png)');
     canvas.css('width', this.goban.width);
     canvas.css('height', this.goban.height);
+    canvas[0].width = this.goban.width;
+    canvas[0].height = this.goban.height;
+    this.context = canvas[0].getContext('2d');
 
     this.coordinatesToPosition = function(arr) {
       // Учитываем краевые позиции, чтобы камень нельзя было поставить дальше поля
-      var x = Math.round((arr[0] - 23) / 44);
+      var x = Math.round((arr[0] - this.goban.frameX) / this.goban.cellsize);
       if (x > size - 1) {
         x = size - 1;
       }
-      var y = Math.round((arr[1] - 26) / 44);
+      var y = Math.round((arr[1] - this.goban.frameY) / this.goban.cellsize);
       if (y > size - 1) {
         y = size - 1;
       }
@@ -64,13 +76,15 @@
     this.positionToCoordinates = function(arr) {
       // рассчет вида
       // ширина квадрата * смещение + ширина границы + сумма однопиксельных интервалов между квадратами
-      return [44 * arr[0] + 23 + arr[0], 44 * arr[1] + 26 + arr[1]];
+      return [this.goban.cellsize * arr[0] + this.goban.frameX + arr[0], this.goban.cellsize * arr[1] + this.goban.frameY + arr[1]];
     }
 
     this.drawCircle = function(positionArray) {
       var context = this.context;
+      console.log(context);
       context.beginPath();
       var coordArray = this.positionToCoordinates(positionArray);
+      console.log(coordArray);
       context.arc(coordArray[0], coordArray[1], 20, 0, 2 * Math.PI, false);
       context.fillStyle = this.game.activePlayer;
       context.fill();
@@ -103,6 +117,7 @@
 
     this.legalMove = function(position) {
       console.log(this.field[position[0]][position[1]]);
+      console.log("x=" + position[0] + " y=" + position[1]);
       if (this.field[position[0]][position[1]] != undefined) {
         return false;
       } else {
@@ -126,7 +141,6 @@
     });
   };
 
-
 $(document).ready(function() {
-  var game = new Game($('#canvas'), $('#goban'), 19);
+  game = new Game($('#canvas'), $('#goban'), 19);
 });
