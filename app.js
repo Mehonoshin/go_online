4288d83a18c14c2b0d4b1227e89445a329977b75
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-09-30 20:50:17 +0400

    Рефактор socket.io сервере

--- app.js
+++ app.js
@@ -12,29 +12,9 @@ app.listen(8888);
 
 // Transport
 
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-
-app.listen(8889);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/public/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
+var io = require('socket.io').listen(8889);
 io.sockets.on('connection', function (socket) {
   socket.on('step', function (data) {
-    socket.emit('step', {data: "He moved a stone", original: data})
     console.log(data);
   });
 });
-
