69559bff37ee6c23bb73076d2ce2c2481340bb80
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-09-30 19:28:50 +0400

    Вброс socket.io в проект

--- index.js
+++ index.js
@@ -6,7 +6,34 @@ var handle = {};
 handle['/'] = requestHandlers.index;
 handle['/index'] = requestHandlers.index;
 handle['/about'] = requestHandlers.about;
-handle['/auth'] = requestHandlers.auth;
-handle['/game'] = requestHandlers.game;
+handle['/game'] = requestHandlers.about;
 
 server.start(router.route, handle);
+
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8889);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/public/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.on('step', function (data) {
+    socket.emit('step', {data: "He moved a stone", original: data})
+    console.log(data);
+  });
+});
+
