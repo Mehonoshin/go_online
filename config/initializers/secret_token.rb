dcc41fd356f1d7d01c3788df070d958e65874ce7
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-11-30 16:00:58 +0400

    Убрана rails приложуха

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GoOnline::Application.config.secret_token = '7c450065600cf829fcf17186b5a1733969d018cfaa481e7d47fe221c42b443a3bc8a18ab2d7bf4e198f2a26f9eac5c3e47b087e28aa919bb2fa7e2b0f7e78e90'
